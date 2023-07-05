9cfa7cd7f73bad1d1cc095611c0b09117e9938eb
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-07-05 11:38:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+}
 Person.prototype.greet=()=>console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 
 function Employee(name, age, jobTitle) {
-	
+	this.name=name;
+	this.age=age;
+	this.jobTitle=jobTitle;
 }
 Employee.__proto__=Person.__proto__;
 Employee.prototype.jobGreet=()=>console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
