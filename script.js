28a65e67ee952018587439f37b9de3c995ae2cc1
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-07-05 11:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ function Person(name, age) {
 Person.prototype.greet=()=>console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 
 function Employee(name, age, jobTitle) {
-	this.name=name;
-	this.age=age;
+	Person.call(this, name, age); // Call the Person constructor with name and age arguments
+    this.jobTitle = jobTitle;
 	this.jobTitle=jobTitle;
 }
-Employee.__proto__=Person.__proto__;
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 Employee.prototype.jobGreet=()=>console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 // Do not change code below this line
 window.Person = Person;
