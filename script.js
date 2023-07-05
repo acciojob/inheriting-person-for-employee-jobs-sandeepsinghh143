196695d1cf935f1cef7fad4d4697652f84d05e18
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-07-05 11:57:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,5 +14,9 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 Employee.prototype.jobGreet=()=>console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 // Do not change code below this line
+let person=new Person(name,age);
+let emp=new Employee(name,age,jobTitle);
+person.greet();
+emp.jobGreet();
 window.Person = Person;
 window.Employee = Employee;
